52eb684f0cfd2d182ac04fae7c198f8df7650ef9
Ashton Coghlan <ashton.coghlan@gmail.com>
2015-08-11 13:07:19 +0300

    Sets up reputation specs.

---
+++ spec/features/rep_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+require 'spec_helper'
+
+feature 'Stack Overflow Repuation Data' do
+
+  scenario 'pull reputation data from external api successfully'
+  scenario 'pull reputation data from external api unsuccessfully'
+
+  scenario 'sum lagos fellow reps'
+  scenario 'sum nairobi fellow reps'
+  scenario 'sum staff reps'
+
+  scenario 'average lagos fellow reps'
+  scenario 'average nairobi fellow reps'
+  scenario 'average staff reps'
+
+  scenario 'show top 10 lagos fellow +rep users'
+  scenario 'show top 10 nairobi fellow +rep users'
+  scenario 'show top 10 staff +rep users'
+end

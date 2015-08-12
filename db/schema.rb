166653e90426606d320015dfab3c0f758bbbb7b3
Ashton Coghlan <ashton.coghlan@gmail.com>
2015-08-12 12:20:22 +0300

    Changed User attributes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150811085026) do
+ActiveRecord::Schema.define(version: 20150812084340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,10 @@ ActiveRecord::Schema.define(version: 20150811085026) do
     t.string   "encrypted_password", limit: 128, null: false
     t.string   "confirmation_token", limit: 128
     t.string   "remember_token",     limit: 128, null: false
+    t.string   "name"
+    t.integer  "stack_id"
+    t.string   "location"
+    t.string   "user_type"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
